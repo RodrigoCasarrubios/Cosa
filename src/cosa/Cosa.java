601c427cd112672d881909139f3fa67f9f961c9f
Clase <Clase@DESKTOP-CN7RHLU>
2023-11-15 18:39:06 +0100

    Todo en 1

---
+++ src/cosa/Cosa.java
@@ -0,0 +1,12 @@
+package cosa;
+
+public class Cosa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola a todos");
+		System.out.println("y");
+		System.out.println("Hola a todas");
+	}
+
+}
